c3850911ceaa74d2b4d7ff9622d943766b1ebb38
Wiesner András <wabe@t-email.hu>
2022-09-23 20:58:56 +0200

    initial, working version

---
+++ embformat.h
@@ -0,0 +1,8 @@
+/* (C) Wiesner András, 2022 */
+
+#ifndef EMBFORMAT_EMBFORMAT_H
+#define EMBFORMAT_EMBFORMAT_H
+
+unsigned long int embfmt(char *str, unsigned long int len, char *format, ...);
+
+#endif //EMBFORMAT_EMBFORMAT_H
